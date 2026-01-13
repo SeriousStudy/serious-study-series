89e581b1d277517cb0f9751df47b31a6dfaeb2ef
SeriousStudy <piyushpand88@gmail.com>
2026-01-13 20:31:11 +0530

    Build to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/serious-study-series/',
+  base: "/serious-study-series/",
+  build: {
+    outDir: "docs"
+  }
 })
