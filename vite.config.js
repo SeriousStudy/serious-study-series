1d0de4d1fd54b3540aafa6e685e0829a009b1248
SeriousStudy <piyushpand88@gmail.com>
2026-01-13 20:37:51 +0530

    Fix GitHub Pages 404 by using relative base

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/serious-study-series/",
+  base: "./",
   build: {
-    outDir: "docs"
-  }
+    outDir: "docs",
+  },
 })
